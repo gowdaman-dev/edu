2ef57d710458cc6b9d7822cf7a5b0ccf73d6d7f0
deepath15 <deepath1506@gmail.com>
2024-01-23 11:36:45 +0530

    requestform

---
+++ src/app/components/Requestform.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Requestform = () => {
+  return (
+    <div>
+      <p>
+        Note: this form is designed for EDUCATORS only. If you are a STUDENT,
+      <span>please recommend it to your school</span>   instead.
+      </p>
+    </div>
+  );
+};
+
+export default Requestform;
---
+++ src/app/request/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Requestform from '../components/Requestform'
+const page = () => {
+  return (
+    <div>
+      <Requestform />
+    </div>
+  )
+}
+
+export default page
