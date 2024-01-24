4a292a8d8cffefc0f3b6dd76750eee1e4a3a216f
jahangeer7704 <jahangeer7704@gmail.com>
2024-01-24 02:49:32 -0800

    small error

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,42 +1,7 @@
-'use client'
-import { signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
-import LoaderPage from '../components/loader/LoadingPage';
+import React from 'react'
+import UserInfo from '../components/UserInfo'
 function page() {
-  const [userId, setUserId] = useState();
-  const router = useRouter();
-  const { data: session } = useSession();
-  useEffect(() => {
-    const userinfoga = async () => {
-      try {
-        const resUserExists = await fetch("/api/userinfo", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({ email:session?.user?.email })
-        })
-        const { user } = await resUserExists.json()
-        if (user) {
-          setUserId(user)
-          router.replace(`/dashboard/${userId['_id']}`)
-          console.log(user);
-          return
-        } else {
-          signOut()
-          console.log('error');
-        }
-      } catch (error) {
-        console.log("error :", error);
-      }
-    }
-    userinfoga()
-  })
-  console.log(userId);
-  return (
-    <LoaderPage/>
-  )
+  return (<UserInfo/>)
 }
 
 export default page
--- src/middleware.js
+++ src/middleware.js
@@ -1,2 +1,2 @@
 export {default} from "next-auth/middleware"
-export const config = {matcher:["/dashboard","/dashboard/:user*"]}
+export const config = {matcher:["/dashboard"]}
