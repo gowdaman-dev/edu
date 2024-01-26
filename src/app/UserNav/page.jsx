05ee4e90777f1c575d6093b6af33e4eb25bb1809
hariharan7080 <hariharank007089@gmail.com>
2024-01-26 15:41:22 +0530

    fix : Add Navbar

---
+++ src/app/UserNav/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navigator from '../components/Navbar/Navigator'
+
+
+function page() {
+  return (
+    <div>
+        <Navigator />
+
+        
+        
+        
+      
+    </div>
+  )
+}
+
+export default page
