d4560b4370ad4efa5e3cc7a24eb3afc94f002d19
gowdaman-dev <damangowdaman@gmail.com>
2024-02-01 21:43:29 +0530

    new

--- src/app/components/Navbar/AdminNavbar.jsx
+++ src/app/components/Navbar/AdminNavbar.jsx
@@ -2,23 +2,28 @@
 import { InlineIcon } from '@iconify/react'
 import { signOut } from 'next-auth/react'
 import Image from 'next/image'
+import { TiUser } from "react-icons/ti";
+import { IoLibrary } from "react-icons/io5";
+import { BiSolidSchool } from "react-icons/bi";
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import React, { useContext, useEffect, useRef, useState } from 'react'
-const navlinks = [
+const adminlinks = [
     {
         label: 'Member List',
-        icon: <Image alt='' src={'/icons/nav/frame.svg'} height={24} width={24} />,
+        icon: <TiUser />,
         path: '/dashboard'
     },
     {
         label: 'Shared Library',
-        icon: <Image alt='' src={'/icons/nav/bookmark.svg'} height={24} width={24} />,
+        icon: <IoLibrary />,
         path: '/dashboard/library'
     },
+]
+const navlinks = [
     {
         label: 'School Information',
-        icon: <Image alt='' src={'/icons/nav/note-2.svg'} height={24} width={24} />,
+        icon: <BiSolidSchool />,
         path: ''
     },
     {
@@ -28,7 +33,7 @@ const navlinks = [
     },
 ]
 function AdminNavbar() {
-    const router = useRouter()
+    const path = usePathname()
     const [adder, setAdder] = useState(false)
     const addermenuref = useRef()
     const addmenuref = useRef()
@@ -46,12 +51,12 @@ function AdminNavbar() {
     })
 
     return (
-        <div className={`min-w-[280px] border-r h-full overflow-hidden transition-all duration-500 flex flex-col gap-6 justify-start`}>
+        <div className={`min-w-[280px] border-r h-full overflow-hidden transition-all duration-500 flex flex-col gap-6 py-4 justify-start`}>
             <ul className='relative flex w-full items-center font-light flex-col  justify-center'>
-                <button ref={addmenuref} onClick={() => setAdder(!adder)} className='bg-white px-2 rounded-lg text-[--web-primary-color] hover:text-teal-400 shadow-[0px_0px_4px_0px] shadow-[--web-primary-color] w-full py-2 text-xl flex items-center justify-center gap-3'><InlineIcon icon="ph:plus-bold" height="20" width="20" /><span>Add Member</span></button>
+                <button ref={addmenuref} onClick={() => setAdder(!adder)} className='bg-white px-2 rounded-lg text-[--web-primary-color] hover:text-teal-400 shadow-[0px_0px_4px_0px] shadow-[--web-primary-color] w-[80%] py-2 text-xl flex items-center justify-center gap-3'><InlineIcon icon="ph:plus-bold" height="20" width="20" /><span>Add Member</span></button>
                 {
                     adder && (
-                        <div ref={addermenuref} className="absolute -bottom-[300%] py-2 px-4 bg-white flex flex-col gap-2 bg-transparent mt-3 z-[2] rounded-lg shadow">
+                        <div ref={addermenuref} className="absolute -bottom-[200%] py-2 w-[80%] px-4 bg-white flex flex-col gap-2 bg-transparent mt-3 z-[2] rounded-lg shadow">
                             <button>Add Manually</button>
                             <button>Request</button>
                         </div>
@@ -60,18 +65,33 @@ function AdminNavbar() {
             </ul>
             {
                 navlinks.map((links) => {
-                    return <Link
-                        href={links?.path}
-                        className='w-[90%] h-fit text-[--text-primary] hover:bg-gray-200 justify-start transition-color duration-500 py-4 rounded-r-full text-left px-4 flex  gap-2 items-center'>
-                        <span>
-                            {links.icon}
-                        </span>
-                        <span>
-                            {
-                                links.label
-                            }
-                        </span>
-                    </Link>
+                    if (path == links.path) {
+                        return <Link
+                            href={links?.path}
+                            className='w-[90%] h-fit text-white bg-[--web-primary-color] justify-start transition-color duration-500 py-4 rounded-r-full text-left px-4 flex  gap-2 items-center'>
+                            <span className='text-white text-2xl'>
+                                {links.icon}
+                            </span>
+                            <span>
+                                {
+                                    links.label
+                                }
+                            </span>
+                        </Link>
+                    } else {
+                        return <Link
+                            href={links?.path}
+                            className='w-[90%] h-fit text-[--text-primary] hover:bg-gray-200 justify-start transition-color duration-500 py-4 rounded-r-full text-left px-4 flex  gap-2 items-center'>
+                            <span className='text-2xl'>
+                                {links.icon}
+                            </span>
+                            <span>
+                                {
+                                    links.label
+                                }
+                            </span>
+                        </Link>
+                    }
                 })
             }
             <button className='w-fit text-[--text-primary] hover:bg-gray-200 justify-start transition-color duration-500 py-2 rounded-lg text-left px-4 flex  gap-2 items-center' onClick={() => signOut()}> <Image alt='' src={'/icons/nav/login.svg'} height={24} width={24} /> <span>SignOut</span></button>
--- src/app/components/Navbar/Navigator.jsx
+++ src/app/components/Navbar/Navigator.jsx
@@ -1,17 +1,72 @@
 'use client'
 import Image from 'next/image'
-import React, { useContext} from 'react'
+import React, { useContext, useState } from 'react'
 import { InlineIcon } from '@iconify/react'
 import AdminNavbar from './AdminNavbar'
 import { UserContext } from '@/ContextUser'
 import { motion } from 'framer-motion'
+const grade = [
+    {
+        label:'grade 1',
+        value:1,
+    },
+    {
+        label:'grade 2',
+        value:2,
+    },
+    {
+        label:'grade 3',
+        value:3,
+    },
+    {
+        label:'grade 4',
+        value:4,
+    },
+    {
+        label:'grade 5',
+        value:5,
+    },
+    {
+        label:'grade 6',
+        value:6,
+    },
+    {
+        label:'grade 7',
+        value:7,
+    },
+    {
+        label:'grade 8',
+        value:8,
+    },
+    {
+        label:'grade 9',
+        value:9,
+    },
+    {
+        label:'grade 11',
+        value:11,
+    },
+    {
+        label:'grade 12',
+        value:12,
+    },
+    {
+        label:'grade 1',
+        value:1,
+    },
+    {
+        label:'grade 1',
+        value:1,
+    },
+]
 function Navigator({ children }) {
-    const {setnav, nav} = useContext(UserContext)
+    const { setnav, nav } = useContext(UserContext)
+    const [grade, setGrade] = useState(grade[0])
     return (
         <div className='w-screen h-full'>
             <div className="flex px-10 py-4 justify-between items-center">
                 <div className=" flex items-center justify-center gap-2">
-                    <Image onClick={()=>setnav(!nav)} className='' src={'/icons/menu.svg'} height={30} width={30} alt='menu' />
+                    <Image onClick={() => setnav(!nav)} className='' src={'/icons/menu.svg'} height={30} width={30} alt='menu' />
                     <div className=" flex items-center justify-center gap-2">
                         <Image src={'/logos/logo.svg'} height={30} width={30} alt='logo' />
                         <h1 className='font-bold tex-gray-900 text-lg'>Edulearn</h1>
@@ -22,14 +77,11 @@ function Navigator({ children }) {
                     <input type="text" className='w-[300px] bg-transparent outline-none px-2 py-1 text-gray-700' placeholder='Search' />
                 </div>
                 <div className="flex">
-                    <select className='px-2 py-2 rounded-lg bg-white shadow-[0px_0px_2px_0px_var(--green-mild)] ' name="" id="">
-                        <option value="1">Grade 1 </option>
-                        <option value="add"> Add Grade </option>
-                    </select>
+                    <button>{grade['label']}</button>
                 </div>
             </div>
             <div className="flex h-full justify-end">
-                <motion.div animate={nav?{width:'280px'}:{width:'0px'}} className="h-full flex justify-end">
+                <motion.div animate={nav ? { width: '280px'} : { width: '0px' }} className="h-full flex justify-end">
                     <AdminNavbar />
                 </motion.div>
                 <div className="w-[100%] min-h-screen">
