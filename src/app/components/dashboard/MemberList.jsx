08698c81910c7c6e360c8e001217b2671db6a90b
gowdaman-dev <damangowdaman@gmail.com>
2024-02-04 00:56:37 +0530

    new

--- src/app/components/dashboard/MemberList.jsx
+++ src/app/components/dashboard/MemberList.jsx
@@ -1,4 +1,8 @@
-export const fetchCache = 'force-no-store';
+export const dynamic = 'auto'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+export const runtime = 'nodejs'
+export const preferredRegion = 'auto'
 async function EventHandler() {
     const res = await fetch('/api/memberlist',{ cache: 'no-store', next: { revalidate: 0 }})
     const data = await res.json();
