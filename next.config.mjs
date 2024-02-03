b63cb25c3e5c47298cc3d8366396d7aac810745c
gowdaman-dev <damangowdaman@gmail.com>
2024-02-04 00:48:23 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'no-store',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
