5cbf935a693cdc1b5e171fb015a1069571142060
gowdaman-dev <damangowdaman@gmail.com>
2024-02-04 00:29:58 +0530

    new

--- src/app/components/dashboard/MemberList.jsx
+++ src/app/components/dashboard/MemberList.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import axios from 'axios';
 async function EventHandler() {
-    const res = await axios.get('/api/memberlist')
+    const res = await axios.get('/api/memberlist',
+        {
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Expires': '0',
+            }
+        })
     const data = await res.data;
     return data;
 }
