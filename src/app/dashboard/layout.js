fcb1dbc2824d6ad3866f67337f0bd152d5bb54ce
Gowdaman p <damangowdaman@gmail.com>
2024-02-05 04:26:55 -0600

    exporter

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import NavBar from '../components/navigator/NavBar'
 import SideNav from '../components/navigator/SideNav'
-import SideNavMob from '../components/navigator/SideNavMob'
 
 function layout({ children }) {
     return (
         <div className='max-w-screen overflow-hidden'>
             <NavBar />
             <div className="flex h-screen max-w-screen">
-                <SideNav/>
-                <SideNavMob/>
-                <div className="h-screen max-w-screen">
+                <SideNav />
+                <div className="h-screen md:w-full">
                     {children}
                 </div>
             </div>
