1fa971f80f335d2aef5df484174571ea6d0b4b56
gowdaman-dev <damangowdaman@gmail.com>
2024-02-12 04:06:17 -0600

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <div className='w-screen'>
       <div className="nav w-screen py-2 flex justify-between">
-        <Image src={'/mailimg.jpg'} height={80} width={100}></Image>
+        <Image src={'/mailimg.jpg'} height={240} hidden width={850}></Image>
       </div>
     </div>
   )
