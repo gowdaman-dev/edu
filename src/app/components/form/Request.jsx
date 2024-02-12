7ca1548ac6b7b68beabcbe5a2bdf8e7ed57ef1cd
deepath15 <deepath1506@gmail.com>
2024-02-12 23:39:17 +0530

    rework : request page

--- src/app/components/form/Request.jsx
+++ src/app/components/form/Request.jsx
@@ -1,28 +1,58 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { webName } from "../globalDetails";
 import DropDown from "./DropDown";
 import { grades } from "./grade";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Requestform = () => {
-  const schoolName = [
+  const schoolname = [
     {
-      "school": "acet",
+      school: "acet",
     },
     {
-      "school": "achariya",
+      school: "achariya",
     },
   ];
   const userType = [
     {
-      "role": "student",
+      role: "student",
     },
     {
-      "role": "staff",
+      role: "staff",
     },
   ];
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState({
+    userName: "",
+    email: "",
+    schoolName: "",
+    role: "",
+    grade: "",
+    comment: "",
+  });
+  const { schoolName } = data;
+  const dropdownRef = useRef();
+  //close dropdown when click happended outside of the field
+  useEffect(() => {
+    const handleClose = (e) => {
+      if (e.target != dropdownRef.current) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("click", handleClose);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const toggle = () => {
+    setIsOpen(true);
+  };
+
   const regualarClass =
     "rounded-[3px] capitalize pl-2 w-72 text-b h-12 border outline-none focus:border-[3px] border-[--web-primary-color] bg-[--web-container]";
   const roleClass =
@@ -30,14 +60,9 @@ const Requestform = () => {
 
   const [isStudent, setisStudent] = useState(false); // State to store input field value of second dropdown
 
-
   const handlerole = (value) => {
-  
     value.trim() === "student" ? setisStudent(true) : setisStudent(false);
   };
-  const handleSubmit = e =>{
-    e.preventDefault();
-  }
 
   const Comment =
     "Tell us more about yourself and the purpose of using our product";
@@ -73,12 +98,56 @@ const Requestform = () => {
                   required
                   className="rounded-[3px] pl-2 h-12 outline-none focus:border-[3px] border border-[--web-primary-color] bg-[--web-container]"
                 />
+                {
+                  <div className="relative ">
+                    <div
+                      ref={dropdownRef}
+                      className="rounded-[3px] cursor-pointer capitalize pl-2 py-3 w-72 text-b h-12 border outline-none focus:border-[3px] border-[--web-primary-color] bg-[--web-container]"
+                      onChange={(e) => setData({ ...data, schoolName })}
+                      onClick={toggle}
+                    >
+                      {" "}
+                      { {schoolName} || "School Name"} {" "}
+                    </div>
 
-                <DropDown
-                  options={schoolName}
-                  className={regualarClass}
-                  default={"School Name"}
-                />
+                    <AnimatePresence mode="wait">
+                      {isOpen && (
+                        <motion.div
+                          initial={{ y: 10, opacity: 0.6 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          exit={{ y: 10, opacity: 0 }}
+                          transition={{ duration: 0.5, type: "spring" }}
+                          className="absolute max-h-64 overflow-auto w-72 mt-2 z-40 pl-2 py-2  rounded-lg grid gap-2 bg-white round"
+                        >
+                          {" "}
+                          
+                          {  
+                              schoolname.map((option) => {
+                              const label = Object.keys(option).map(
+                                (key) => option[key]
+                              );
+
+                              return (
+                                <div>
+                                  <p>School Name</p>
+                                <p
+                                  className="capitalize cursor-pointer p-1 w-[273px] rounded-lg hover:bg-gray-100"
+                                  onClick={ () => setData({...data,schoolName: label}) }
+                                  key={label}
+                                >
+                                  {" "}
+                                  {label}
+                                </p>
+                                </div>
+                              );
+                            })}
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
+                   
+                  </div>
+                }
+                 {console.log({schoolName})}
               </div>
               <div className="flex flex-col gap-10 w-72 ">
                 <input
@@ -108,7 +177,6 @@ const Requestform = () => {
           <div className="flex justify-center gap-10 flex-col py-10 ">
             <textarea
               type="text"
-              
               placeholder={Comment}
               className="rounded-[3px] pl-2 pt-1 min-h-56 outline-none focus:border-[3px] h-auto w-72 md:w-[600px] mx-auto  border border-[--web-primary-color] bg-[--web-container]"
             />
