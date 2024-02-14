ef0103148c04f756bc0d8b5674e377779c4f9f8b
gowdaman-dev <damangowdaman@gmail.com>
2024-02-14 11:16:42 -0600

    home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,37 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { FaCircleChevronRight } from 'react-icons/fa6'
 function page() {
   return (
-    <div className='w-screen'>
-      <div className="nav w-screen py-2 flex justify-between">
+    <div className='w-screen h-fit'>
+      <div className="nav w-screen flex justify-between">
         <Image src={'/mailimg.jpg'} height={240} hidden width={850}></Image>
       </div>
+      <div className="homenav fixed px-4 w-screen py-4 border-b flex items-center justify-between">
+        <div className="flex items-center justify-center gap-2">
+          <Image src={'/logos/logo.svg'} height={40} width={40} />
+          <h1 className='text-lg font-semibold text-[--web-primary-color]'>EDULEARN</h1>
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <Link className='p-2 px-4 tracking-widest text-white hover:text-gray-300 transition-color duration-5000 ease' href={'#home'}>Home</Link>
+          <Link className='p-2 px-4 tracking-widest text-white hover:text-gray-300 transition-color duration-5000 ease' href={'#service'}>Service</Link>
+          <Link className='p-2 px-4 tracking-widest text-white hover:text-gray-300 transition-color duration-5000 ease' href={'#contact'}>Content</Link>
+          <Link className='p-2 px-4 tracking-widest shadow rounded-xl hover:text-purple-800 hover:bg-white transition-color duration-500 ease-in-out text-white border border-white' href={'/signin'}>SignIn</Link>
+        </div>
+      </div>
+      <section id="home" className='w-screen min-h-screen flex flex-row px-4 bg-[url(/hero_shape.png)] bg-cover bg-purple-800 items-center justify-between'>
+        <div className="w-[50%] h-full grid place-items-center">
+          <div className="flex flex-col items-start gap-2">
+            <h1 className='text-6xl tracking-wide text-purple-900 font-black'>Welcome <br />To Our Best <br /> E Learning Platform </h1>
+            <Link href={'/recomend'} className='flex items-center justify-center gap-2 px-6 py-4 mt-2 text-white rounded-full bg-purple-800'>Get Start your journey<FaCircleChevronRight /></Link>
+            <p className='px-6'>Are You A User? <Link href={'signin'}>SignIn here!</Link></p>
+          </div>
+        </div>
+        <div className="w-[50%] h-full">
+          <Image src={'/homeimg.svg'} width={200} height={200} className='h-full w-full' />
+        </div>
+      </section>
     </div>
   )
 }
--- src/app/resetpassword/[user]/page.jsx
+++ src/app/resetpassword/[user]/page.jsx
@@ -70,10 +70,10 @@ function page({ params }) {
                 <h1 className='text-purple-800 text-6xl uppercase font-bold'>Edulearn</h1>
                 <h2 className='text-2xl text-gray-900 font-semibold'>Reset your Password</h2>
                 <p className='text-gray-800 font-[200] text-[12px]'>Caution : don't share your reset password link .</p>
-                <form onSubmit={resetPassword} action="" className='flex flex-col items-center justify-center gap-4 w-full' method="post">
-                    <input onChange={(e) => setoldpass(e.target.value)} type="password" className='md:w-[300px] rounded-lg w-full outline-none border p-2' placeholder='Current Password' />
-                    <input onChange={(e) => setCurrentpass({ ...currentpass, password: e.target.value })} type="password" className='md:w-[300px] rounded-lg w-full outline-none border p-2' placeholder='New Password' />
-                    <input onChange={(e) => setCurrentpass({ ...currentpass, conform: e.target.value })} type="password" className='md:w-[300px] rounded-lg w-full outline-none border p-2' placeholder='Conform New Password' />
+                <form onSubmit={resetPassword} className='flex flex-col items-center justify-center gap-4 w-full' method="post">
+                    <input required aria-label='reset pasword edulearn' onChange={(e) => setoldpass(e.target.value)} type="password" className='md:w-[300px] rounded-lg w-full outline-none border p-2' placeholder='Current Password' />
+                    <input required aria-label='reset pasword edulearn' onChange={(e) => setCurrentpass({ ...currentpass, password: e.target.value })} type="password" className='md:w-[300px] rounded-lg w-full outline-none border p-2' placeholder='New Password' />
+                    <input required aria-label='reset pasword edulearn' onChange={(e) => setCurrentpass({ ...currentpass, conform: e.target.value })} type="password" className='md:w-[300px] rounded-lg w-full outline-none border p-2' placeholder='Conform New Password' />
                     <input type="submit" className='cursor-pointer md:w-[300px] rounded-lg w-full outline-none border p-2 bg-purple-800 text-white' value={status?status:'reset password'} />
                 </form>
             </div>
