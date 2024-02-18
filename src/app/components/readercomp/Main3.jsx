4fc773c9816a6dde4b9ac69a3fdb18259037f8a9
jahangeer7704 <jahangeer7704@gmail.com>
2024-02-18 17:16:16 +0530

    test

--- src/app/components/readercomp/Main3.jsx
+++ src/app/components/readercomp/Main3.jsx
@@ -3,7 +3,7 @@ import { getDownloadURL, ref } from 'firebase/storage'
 import axios from 'axios'
 export async function PdfFetch() {
     try {
-        const bytes = await axios.get("https://firebasestorage.googleapis.com/v0/b/lmsedu-e5dbc.appspot.com/o/files%2Fd9eaa0d5-de5c-4cb5-be82-cc9502a9c238?alt=media&token=64d1a4d8-ec13-478d-a295-743190c79526",{
+        const bytes = await axios.get("https://firebasestorage.googleapis.com/v0/b/lmsedu-e5dbc.appspot.com/o/files%2Fca97e39c-ca5e-4060-8237-83c36dbfdc86?alt=media&token=1418003f-969d-41a4-976c-04a4bcf2d5b4",{
             method:'GET',
             responseType:'arraybuffer'
         })
