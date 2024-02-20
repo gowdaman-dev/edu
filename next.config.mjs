d347c15af3592bd166fed6f409ff33ffef394aed
jahangeer7704 <jahangeer7704@gmail.com>
2024-02-20 10:59:10 +0530

    firebase err

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+import * as path from 'path';
 const nextConfig = {
     webpack: (config) => {
         config.module.rules.push({
           test: /\.node/,
-          exclude:/node_modules\/(firebase|@firebase)\//,
+          exclude:
+          [
+            path.resolve(process.cwd(), 'node_modules', 'firebase'),
+            path.resolve(process.cwd(), 'node_modules', '@firebase'),
+          ],
           use: "raw-loader",
         });
     return config;
