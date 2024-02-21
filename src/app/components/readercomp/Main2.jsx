09d1c8122042256933c31fe2a234e4a3b54704e7
jahangeer7704 <jahangeer7704@gmail.com>
2024-02-21 20:13:41 +0530

    test bug on reader

--- src/app/components/readercomp/Main2.jsx
+++ src/app/components/readercomp/Main2.jsx
@@ -1,36 +1,45 @@
-"use client";
+"use client"
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
-import { useEffect, useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { UserContext } from "@/ContextUser";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { PdfFetch } from "./Main3";
-const PdfViewer = () => {
-  const {url}=useContext(UserContext)
+import LoaderPage from "../loader/LoadingPage";
 
-    const [buffer, setBuffer] = useState([])
-    useEffect(() => {
-        async function fetchBytes() {
-            try {
-                const data = await PdfFetch(url);
-                console.log(data);
-                 setBuffer(data);
-            } catch (error) {
-                console.log(error.message);
-            }
-        }
-        fetchBytes();
-    },[]);
+const PdfViewer = () => {
+  const { url } = useContext(UserContext);
+  const [buffer, setBuffer] = useState([]);
+  const [loading, setLoading] = useState(true); // Track loading state
+
+  useEffect(() => {
+    async function fetchBytes() {
+      try {
+        const data = await PdfFetch(url);
+        setBuffer(data);
+        setLoading(false); 
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchBytes();
+  }, [url]);
+
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
-    <div className="h-screen w-screen">
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <Viewer
-          fileUrl={buffer}
-        />
-      </Worker>
-    </div>
+    <>
+      {loading && <LoaderPage />}
+      {buffer.length > 0 && (
+        <div className="h-screen w-screen">
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+            <Viewer fileUrl={buffer} />
+          </Worker>
+        </div>
+      )}
+    </>
   );
 };
+
 export default PdfViewer;
